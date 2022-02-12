174692041416bd1d89a6d015cb229f1f6128198d
Divya Syal <divya.s@affinidi.com>
2022-02-12 11:11:43 -0800

    initial commit for empty app just created - Divya

---
+++ ProjectPineapple/ProjectPineapple/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectPineapple
+//
+//  Created by Divya Syal on 2/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
